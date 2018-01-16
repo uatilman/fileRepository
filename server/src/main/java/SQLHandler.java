8b4c0c4b12b5c27cb7980649bcb2dd1ab69ed7ba
YURY TILMAN <uatilman@gmail.com>
2018-01-16 12:43:18 +0300

    updateDBAndAddMethod_getPassByLogin

--- server/src/main/java/SQLHandler.java
+++ server/src/main/java/SQLHandler.java
@@ -1,22 +1,26 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class SQLHandler {
     Connection connection;
-    PreparedStatement ps;
+
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         SQLHandler sqlHandler = new SQLHandler();
         sqlHandler.connection();
         sqlHandler.insertTestDate();
-        sqlHandler.disConnect();
+        sqlHandler.disconnect();
 
     }
-
+    public String getPassByLogin(String login, String pass) throws SQLException {
+        PreparedStatement ps = connection.prepareStatement("SELECT nick FROM users WHERE login = ?");
+        ps.setString(1, login);
+        ResultSet rs = ps.executeQuery();
+        if (rs.next())
+            return rs.getString(1);
+        return null;
+    }
     public void insertTestDate() throws SQLException {
-
+       PreparedStatement ps = connection.prepareStatement("INSERT INTO users (login, password) VALUES (?, ?);");
 //        connection.setAutoCommit(false);
         for (int i = 1; i < 16; i++) {
             ps.setString(1, ("user" + i));
@@ -24,20 +28,20 @@ public class SQLHandler {
             ps.execute();
 //            ps.addBatch();
         }
-
+        ps.close();
 //        ps.executeBatch();
 //        connection.setAutoCommit(true);
     }
 
     public void connection() throws ClassNotFoundException, SQLException {
         Class.forName("org.sqlite.JDBC");
-        connection = DriverManager.getConnection("jdbc:sqlite:server/users.db");
-        ps = connection.prepareStatement("INSERT INTO students (login, password) VALUES (?, ?);");
+        connection = DriverManager.getConnection("jdbc:sqlite:server/fileRepository.db");
+
 
     }
 
-    public void disConnect() throws ClassNotFoundException, SQLException {
-        ps.close();
+    public void disconnect() throws ClassNotFoundException, SQLException {
+
         connection.close();
     }
 
